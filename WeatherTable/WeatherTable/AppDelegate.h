e42c24680934a26d721b4e4b8cff29494866b384
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2026-03-22 15:31:24 +0900

    UITableView

---
+++ WeatherTable/WeatherTable/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WeatherTable
+//
+//  Created by Seojihyeon on 3/19/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WeatherTable/WeatherTable/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WeatherTable
+//
+//  Created by Seojihyeon on 3/19/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITableViewDataSource>{
+    NSArray *datalist;
+}
+@end
+
