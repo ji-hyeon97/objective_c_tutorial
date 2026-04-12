e02599b8e28c39928748e03447a4803d6504cd83
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2026-04-12 11:01:17 +0900

    Life cycle, alert, View state callback method

---
+++ LoginFlow/LoginFlow/ LoginInfo.swift
@@ -0,0 +1,14 @@
+//
+//   LoginInfo.swift
+//  LoginFlow
+//
+//  Created by Seojihyeon on 4/12/26.
+//
+
+
+import Foundation
+
+struct LoginInfo {
+    let email: String
+    let password: String
+}
---
+++ LoginFlow/LoginFlow/InfoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  InfoViewController.swift
+//  LoginFlow
+//
+//  Created by Seojihyeon on 4/12/26.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    // MARK: - Properties
+    var loginInfo: LoginInfo?
+    
+    // MARK: - Methods
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("InfoViewController 객체의 뷰가 메모리에 로드됨")
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "로그인 정보"
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("InfoViewController 객체의 뷰가 나타날 예정")
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        guard let info = self.loginInfo else {
+            return
+        }
+        
+        print(info)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("InfoViewController 객체의 뷰가 나타남")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("InfoViewController 객체의 뷰가 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("InfoViewController 객체의 뷰가 사라짐")
+    }
+}
